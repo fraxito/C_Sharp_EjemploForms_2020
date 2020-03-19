9ecd2f07b75bc916e534b82649fcda6c2bfa9d96
fraxito <fraxito@gmail.com>
2020-03-20 00:01:30 +0100

    Cambiado el nombre de los botones

--- EjemploForms/VentanaPrincipal.Designer.cs
+++ EjemploForms/VentanaPrincipal.Designer.cs
@@ -40,7 +40,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "calendario";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.botonVentana01);
             // 
             // button2
             // 
@@ -50,7 +50,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "max";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.botonVentana02);
             // 
             // VentanaPrincipal
             // 
